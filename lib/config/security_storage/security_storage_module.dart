e217e1453403f179970846a21a0266104e19c5bc
Khalid Adel <95598763+khalidadel365@users.noreply.github.com>
2026-05-05 07:42:57 +0300

    Revert "Feature/init project"

--- lib/config/security_storage/security_storage_module.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:injectable/injectable.dart';
-
-@module
-abstract class SecurityStorageModule {
-  @lazySingleton
-  FlutterSecureStorage get secureStorage => const FlutterSecureStorage();
-}
