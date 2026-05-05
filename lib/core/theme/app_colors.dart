7110460189bdefb72f588181b71cf0fd5534079a
Khalid Adel <khalidadel209@gmail.com>
2026-05-05 05:37:38 +0300

    finish theme file & app colors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -9,7 +9,7 @@ abstract class AppColors {
   static const secondaryColor = Color(0xFFF9ECF0);
   static const errorColor = Color(0xFFCC1010);
   static const successColor = Color(0xFF0CB359);
-  static const placeHolderColor = Color(0xFF535353);
-  static const greyColor = Color(0xFFA6A6A6);
+  static const greyColor = Color(0xFF535353);
+  static const placeHolderColor = Color(0xFFA6A6A6);
   static const transparentColor = Colors.transparent;
 }
