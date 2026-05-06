bb4480a3ae6cb954427331de5fa78bcee346774a
KerlosGirgis <kerlos0@protonmail.com>
2026-05-06 20:48:38 +0300

    feat: implement user registration feature

---
+++ lib/features/signup/data/data_sources/register_remote_datasource_contract.dart
@@ -0,0 +1,8 @@
+
+
+import '../model/register_request.dart';
+import '../model/register_response.dart';
+
+abstract interface class RegisterRemoteDatasourceContract {
+  Future<RegisterResponse> register(RegisterRequest request);
+}
