04db17cee8f43ee5d75f7c9f5dbb8fd623bdfd82
Khalid Adel <khalidadel209@gmail.com>
2026-05-08 23:04:31 +0300

    add dio interceptor

--- lib/core/values/app_strings.dart
+++ lib/core/values/app_strings.dart
@@ -3,6 +3,9 @@ abstract class AppStrings {
 
   //auth
   static const String login = 'Login';
+  static const String noToken = 'noToken';
+  static const String authorization = 'Authorization';
+  static const String bearer = 'Bearer';
   static const String loginSuccessfully = 'Login Successfully';
   static const String email = 'Email';
   static const String emailKey = 'email';
