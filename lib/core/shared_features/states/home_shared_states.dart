4b6a6ad8ec2707f71e9b3ba3720843bb5e846584
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-05-13 02:24:33 +0300

    Refactor HomeSharedCubit; clean up CustomProductCard widget formatting

--- lib/core/shared_features/states/home_shared_states.dart
+++ lib/core/shared_features/states/home_shared_states.dart
@@ -3,15 +3,14 @@ import 'package:equatable/equatable.dart';
 class HomeSharedStates extends Equatable {
   //example for state class
   // final BaseState<CategoryEntity> categoriesState;
-  
-  const HomeSharedStates(
-    );
-    // HomeSharedStates copyWith({
-    // }) {
-    //   return HomeSharedStates(
-        
-    //   );
-    // }
+
+  const HomeSharedStates();
+  // HomeSharedStates copyWith({
+  // }) {
+  //   return HomeSharedStates(
+
+  //   );
+  // }
 
   @override
   List<Object> get props => [];
