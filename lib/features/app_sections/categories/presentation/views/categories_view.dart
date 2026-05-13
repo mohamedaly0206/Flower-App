28015c4151f7bc1025641b9b2b388fceb2f4e755
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-05-14 00:37:28 +0300

    feat(categories): add CustomTabBar for tab navigation and update GridView layout

--- lib/features/app_sections/categories/presentation/views/categories_view.dart
+++ lib/features/app_sections/categories/presentation/views/categories_view.dart
@@ -3,10 +3,11 @@ import 'package:flower_app/core/shared_features/Intent/home_shared_intent.dart';
 import 'package:flower_app/core/shared_features/cubit/home_shared_cubit.dart';
 import 'package:flower_app/core/shared_features/states/home_shared_states.dart';
 import 'package:flower_app/core/widgets/custom_product_card.dart';
-import 'package:flower_app/features/app_sections/categories/presentation/widgets/category_tab.dart';
+import 'package:flower_app/core/widgets/custom_tab_bar.dart';
 import 'package:flower_app/features/app_sections/categories/presentation/widgets/search_and_filter_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class CategoriesView extends StatelessWidget {
   const CategoriesView({super.key});
@@ -26,33 +27,45 @@ class CategoriesView extends StatelessWidget {
               const SizedBox(height: 16),
               BlocBuilder<HomeSharedCubit, HomeSharedStates>(
                 builder: (context, state) {
-                  return SizedBox(
-                    height: 40,
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return CategoryTab(
-                          isSelected: true,
-                          categoryName: state.categoriesState.data?.categories[index].name ?? '',
-                        );
-                      },
-                      shrinkWrap: true,
-                      itemCount: state.categoriesState.data?.categories.length ?? 0,
-                      scrollDirection: Axis.horizontal,
-                    ),
+                  return CustomTabBar(
+                    tabs:
+                        state.categoriesState.data?.categories
+                            .map((e) => e.name)
+                            .toList() ??
+                        [],
+                    selectedIndex: state.selectedIndex,
+                    onTabSelected: (index) {
+                      context.read<HomeSharedCubit>().handleHomeSharedIntent(
+                        ChangeTabIntent(index),
+                      );
+                    },
                   );
                 },
               ),
+
               SizedBox(height: 28),
               Expanded(
                 child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: MediaQuery.of(context).size.width > 600
+                        ? 3
+                        : 2,
                     mainAxisSpacing: 18,
                     crossAxisSpacing: 18,
-                    childAspectRatio: 0.7,
+                    childAspectRatio: 0.72,
                   ),
                   itemBuilder: (context, index) {
-                    return CustomProductCard();
+                    return CustomProductCard(
+                      title: 'fuchsia-brilliance-vase',
+                      imageProvider: CachedNetworkImageProvider(
+                        'https://cdn1.1800flowers.com/wcsstore/Flowers/images/catalog/191167xlx.jpg?height=755&width=690?auto=webp',
+                      ),
+                      price: 6000,
+                      oldPrice: 9000,
+                      discountPercent: 33,
+                      onAddToCart: () {},
+                      onTap: () {},
+                    );
                   },
                   shrinkWrap: true,
                   itemCount: 10,
