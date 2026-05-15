0d6ae2e84c314f09f4760cd5f2be63f23c7a1f4b
Khalid Adel <khalidadel209@gmail.com>
2026-05-15 17:35:06 +0300

    feat(home): provide home cubit

--- lib/features/app_sections/home/presentation/view/home_view.dart
+++ lib/features/app_sections/home/presentation/view/home_view.dart
@@ -4,8 +4,11 @@ import 'package:flower_app/features/app_sections/home/presentation/widgets/best_
 import 'package:flower_app/features/app_sections/home/presentation/widgets/categories_list_view.dart';
 import 'package:flower_app/features/app_sections/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../../../core/shared_features/cubit/home_shared_cubit.dart';
+import '../../../../../core/shared_features/states/home_shared_states.dart';
 import '../widgets/occasion_list_view.dart';
 import '../widgets/section_header.dart';
 
@@ -15,70 +18,89 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              child: Row(
-                spacing: 17,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+                child: Row(
+                  spacing: 17,
+                  children: [
+                    Expanded(child: SvgPicture.asset(Assets.icons.logo)),
+                    Expanded(
+                      flex: 3,
+                      child: CustomSearchTextField(
+                        hintText: AppStrings.search,
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
                 children: [
-                  Expanded(child: SvgPicture.asset(Assets.icons.logo)),
-                  Expanded(
-                    flex: 3,
-                    child: CustomSearchTextField(
-                      hintText: AppStrings.search,
-                      onChanged: (value) {},
+                  Icon(
+                    Icons.location_on_outlined,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  const SizedBox(width: 5),
+                  RichText(
+                    text: TextSpan(
+                      text: AppStrings.deliverTo,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                      children: [
+                        TextSpan(
+                          text: AppStrings.dummyAddress,
+                          style: Theme.of(context).textTheme.headlineMedium
+                              ?.copyWith(fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.keyboard_arrow_down_sharp,
+                      color: Theme.of(context).primaryColor,
+                      size: 35,
                     ),
                   ),
                 ],
               ),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Icon(
-                  Icons.location_on_outlined,
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-                const SizedBox(width: 5),
-                RichText(
-                  text: TextSpan(
-                    text: AppStrings.deliverTo,
-                    style: Theme.of(context).textTheme.headlineMedium!,
+              const SizedBox(height: 5),
+              BlocBuilder<HomeSharedCubit, HomeSharedStates>(
+                builder: (context, state) {
+                  return Column(
                     children: [
-                      TextSpan(
-                        text: AppStrings.dummyAddress,
-                        style: Theme.of(context).textTheme.headlineMedium!
-                            .copyWith(fontWeight: FontWeight.w500),
+                      SectionHeader(
+                        title: AppStrings.categories,
+                        onPressed: () {},
                       ),
+                      const CategoriesListView(),
+                      const SizedBox(height: 10),
+                      SectionHeader(
+                        title: AppStrings.bestSeller,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 10),
+                      const BestSellerListView(),
+                      const SizedBox(height: 10),
+                      SectionHeader(
+                        title: AppStrings.occasion,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: 10),
+                      const OccasionListView(),
                     ],
-                  ),
-                ),
-                IconButton(
-                  padding: const EdgeInsets.all(0.0),
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.keyboard_arrow_down_sharp,
-                    color: Theme.of(context).primaryColor,
-                    size: 35,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 5),
-            SectionHeader(title: AppStrings.categories, onPressed: () {}),
-            const CategoriesListView(),
-            const SizedBox(height: 10),
-            SectionHeader(title: AppStrings.bestSeller, onPressed: () {}),
-            const SizedBox(height: 10),
-            const BestSellerListView(),
-            const SizedBox(height: 10),
-            SectionHeader(title: AppStrings.occasion, onPressed: () {}),
-            const SizedBox(height: 10),
-            const OccasionListView(),
-          ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
