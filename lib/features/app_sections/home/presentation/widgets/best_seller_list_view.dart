fff8f520cb0bb6e524471bf8f9e6638974801055
Khalid Adel <khalidadel209@gmail.com>
2026-05-16 17:13:49 +0300

    feat(home): finish best seller list view

--- lib/features/app_sections/home/presentation/widgets/best_seller_list_view.dart
+++ lib/features/app_sections/home/presentation/widgets/best_seller_list_view.dart
@@ -1,22 +1,74 @@
+import 'package:flower_app/core/shared_features/shared_view_model/cubit/home_shared_cubit.dart';
+import 'package:flower_app/core/shared_features/shared_view_model/states/home_shared_states.dart';
 import 'package:flower_app/features/app_sections/home/presentation/widgets/best_seller_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BestSellerListView extends StatelessWidget {
   const BestSellerListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 210,
-      child: ListView.separated(
-        itemCount: 10,
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        separatorBuilder: (context, index) => const SizedBox(width: 10),
-        itemBuilder: (context, index) {
-          return const BestSellerListViewItem();
-        },
-      ),
+    return BlocBuilder<HomeSharedCubit, HomeSharedStates>(
+      buildWhen: (previous, current) =>
+      previous.bestSellersState != current.bestSellersState,
+      builder: (context, state) {
+        final bestSellerState = state.bestSellersState;
+
+        // loading
+        if (bestSellerState.isLoading) {
+          return const SizedBox(
+            height: 210,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        // error
+        if (bestSellerState.errorMessage != null) {
+          return SizedBox(
+            height: 210,
+            child: Center(
+              child: Text(
+                bestSellerState.errorMessage!,
+                style: const TextStyle(color: Colors.red),
+              ),
+            ),
+          );
+        }
+
+        // data
+        final bestSellers = bestSellerState.data ?? [];
+
+        // empty
+        if (bestSellers.isEmpty) {
+          return const SizedBox(
+            height: 210,
+            child: Center(
+              child: Text('No Best Sellers Found'),
+            ),
+          );
+        }
+
+        return SizedBox(
+          height: 210,
+          child: ListView.separated(
+            itemCount: bestSellers.length,
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (context, index) =>
+            const SizedBox(width: 10),
+            itemBuilder: (context, index) {
+              final bestSeller = bestSellers[index];
+
+              return BestSellerListViewItem(
+                bestSeller: bestSeller,
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
--- lib/features/app_sections/home/presentation/widgets/best_seller_list_view_item.dart
+++ lib/features/app_sections/home/presentation/widgets/best_seller_list_view_item.dart
@@ -1,38 +1,50 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flower_app/core/values/app_strings.dart';
+import 'package:flower_app/features/best_seller/domain/models/best_seller_model.dart';
 import 'package:flutter/material.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
+  const BestSellerListViewItem({
+    super.key,
+    required this.bestSeller,
+  });
+
+  final BestSellerModel bestSeller;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CachedNetworkImage(
-          width: 130,
-          height: 150,
-          fit: BoxFit.cover,
-          imageUrl:
-              'https://hips.hearstapps.com/hmg-prod/images/gettyimages-2165950545-69600af8c9b0d.jpg?crop=0.667xw:1.00xh;0.112xw,0&resize=1200:*',
-        ),
-        const SizedBox(height: 8),
-        Text(
-          AppStrings.addNewAddress,
-          style: Theme.of(context).textTheme.bodyMedium,
-        ),
-        Text(
-          AppStrings.addNew,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: Theme.of(
-            context,
-          ).textTheme.headlineMedium?.copyWith(fontWeight: FontWeight.bold),
-        ),
-      ],
+    return SizedBox(
+      width: 130,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CachedNetworkImage(
+            width: 130,
+            height: 150,
+            fit: BoxFit.cover,
+            imageUrl: bestSeller.imgCover ?? '',
+          ),
+
+          const SizedBox(height: 8),
+
+          Text(
+            bestSeller.title ,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 4),
+          Text(
+             '${bestSeller.price} EGP' ,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(
+              context,
+            ).textTheme.headlineMedium?.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
