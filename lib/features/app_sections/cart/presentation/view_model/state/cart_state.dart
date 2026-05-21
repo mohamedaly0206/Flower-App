5ffc1e33b57f96cc1abde068c5d098b5ed55c3c1
mohamedaly0206 <mohamedaly0206@gmail.com>
2026-05-22 02:04:16 +0300

    feat(cart): enhance CartCubit and CartState with intent handling and state management for cart operations

--- lib/features/app_sections/cart/presentation/view_model/state/cart_state.dart
+++ lib/features/app_sections/cart/presentation/view_model/state/cart_state.dart
@@ -1,10 +1,40 @@
-part of '../cubit/cart_cubit.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flower_app/config/base_state/base_state.dart';
+import 'package:flower_app/features/app_sections/cart/domain/entities/cart_response_entity.dart';
 
-abstract class CartState extends Equatable {
-  const CartState();
+class CartState extends Equatable {
+  final BaseState<CartResponseEntity>? getCartItemsState;
+  final BaseState<CartResponseEntity>? addItemToCartState;
+  final BaseState<CartResponseEntity>? removeItemFromCartState;
+  final BaseState<CartResponseEntity>? updateItemQuantityInCartState;
+  const CartState({
+    this.getCartItemsState = const BaseState(),
+    this.addItemToCartState = const BaseState(),
+    this.removeItemFromCartState = const BaseState(),
+    this.updateItemQuantityInCartState = const BaseState(),
+  });
+
+  CartState copyWith({
+    BaseState<CartResponseEntity>? getCartItemsState,
+    BaseState<CartResponseEntity>? addItemToCartState,
+    BaseState<CartResponseEntity>? removeItemFromCartState,
+    BaseState<CartResponseEntity>? updateItemQuantityInCartState,
+  }) {
+    return CartState(
+      getCartItemsState: getCartItemsState ?? this.getCartItemsState,
+      addItemToCartState: addItemToCartState ?? this.addItemToCartState,
+      removeItemFromCartState:
+          removeItemFromCartState ?? this.removeItemFromCartState,
+      updateItemQuantityInCartState:
+          updateItemQuantityInCartState ?? this.updateItemQuantityInCartState,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+    getCartItemsState,
+    addItemToCartState,
+    removeItemFromCartState,
+    updateItemQuantityInCartState,
+  ];
 }
-
-class CartInitial extends CartState {}
