a76acc5edc34e6963c54a1a9df39d23124218bfa
Khalid Adel <khalidadel209@gmail.com>
2026-06-04 05:12:13 +0300

    feat(notification):initialize notification service in main file

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:toastification/toastification.dart';
 import 'config/di/di.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/theme.dart';
+import 'core/utilities/notification_service.dart';
 import 'firebase_options.dart';
 import 'l10n/app_localizations.dart';
 
@@ -21,6 +22,9 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   configureDependencies();
+  final notificationService = NotificationService();
+  await notificationService.initialize();
+
   final initialLocation = await _getInitialLocation();
   final initialLocale = await _getInitialLocale();
   runApp(
